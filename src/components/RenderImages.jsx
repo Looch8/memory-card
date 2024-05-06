236ccf407e9a0631d991758170e22e0c4edc0e62
Luke Wheldale <lukewheldale@gmail.com>
2024-05-06 19:48:32 +0930

    fetch images

---
+++ src/components/RenderImages.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+function RenderGifs() {
+	const [gifImages, setGifImages] = useState([]);
+
+	useEffect(() => {
+		async function fetchGifs() {
+			const response = await fetch(
+				"https://api.giphy.com/v2/emoji?api_key=gI7uLuJvDv5Ukw2hI43fdL6kQ6alVRDU&limit=10&offset=0"
+			);
+			const gifsData = await response.json();
+			setGifImages(gifsData.data);
+		}
+
+		fetchGifs();
+	}, []);
+
+	const renderGifImages = () => {
+		return gifImages.map((gif, index) => (
+			<img key={index} src={gif.images.original.url} alt="gif" />
+		));
+	};
+
+	return <div>{renderGifImages()}</div>;
+}
+
+export default RenderGifs;
